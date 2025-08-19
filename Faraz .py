5783269b3eb7fdc1300f49695ecf03022571840f
Muhammad Faraz Khan <muhammadfarazkhan879@gmail.com>
2025-08-19 03:14:11 +0100

    fourth commit

--- Faraz .py
+++ Faraz .py
@@ -1,13 +1,1 @@
-# Create 4 variables of different datatypes (string, int, float, bool)
-# and print their values along with their types.
-
-a = "Faraz"
-b = 20
-c = 5.9
-d = "false"
-
-print("name:", a,"->", type (a))
-print("age:",b,"->", type(b))
-print("height:",c,"->",type(c))
-print("bool:",d,"->",type(d))
-
+https://github.com/muhammadfarazkhan879-gif/project-phython.githttps://github.com/muhammadfarazkhan879-gif/project-phython.git
